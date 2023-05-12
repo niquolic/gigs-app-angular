f2e33306361a067cbc832bcbf76d1e329943c500
niquolic <nicolas.debruycker22@gmail.com>
2023-05-12 18:45:52 -0400

    menu component

--- gigs-app-angular/src/app/app.component.ts
+++ gigs-app-angular/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from './layouts/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [MenuComponent]
 })
 export class AppComponent {
   title = 'gigs-app-angular';
